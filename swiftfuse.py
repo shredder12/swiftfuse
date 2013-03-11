54238998b85ebd5104b952decdec9264ef5acdfc
Shashank Sahni <shredder12@gmail.com>
2013-03-12 03:18:11 +0530

    ported the code to llfuse

--- swiftfuse.py
+++ swiftfuse.py
@@ -1,18 +1,20 @@
 import errno  
-import fuse  
+import llfuse  
 import stat  
 import time
 import swiftclient
+import logging
 from os import environ
+import sys
+
+log = logging.getLogger()
   
-fuse.fuse_python_api = (0, 2)  
-  
-class swiftfuse(fuse.Fuse):
+class swiftfuse(llfuse.Operations):
     
     swift_conn = None
 
-    def __init__(self, *args, **kw):  
-        fuse.Fuse.__init__(self, *args, **kw)
+    def __init__(self):  
+        super(swiftfuse, self).__init__()
         self.swift_conn = swiftclient.client.Connection(
             authurl=environ['OS_AUTH_URL'],
             user=environ['OS_USERNAME'], 
@@ -21,21 +23,44 @@ class swiftfuse(fuse.Fuse):
             auth_version=2
             )
   
-    def getattr(self, path):  
-        st = fuse.Stat()  
+    def getattr(self, inode):  
+        st = llfuse.EntryAttributes()
+        st.st_ino = inode
+        st.generation = 0
+        st.entry_timeout = 300
+        st.attr_timeout = 300
         st.st_mode = stat.S_IFDIR | 0755  
         st.st_nlink = 2  
         st.st_atime = int(time.time())  
         st.st_mtime = st.st_atime  
-        st.st_ctime = st.st_atime    
+        st.st_ctime = st.st_atime
+        st.st_uid = 1000
+        st.st_gid = 1000
+        st.st_rdev = 0
+        st.st_size = 0
+        st.st_blksize = 512
+        st.st_blocks = 1    
         return st
 
-    def readdir(self, path, offset):
-        for bucket in self.swift_conn.get_account()[1]:
-            yield fuse.Direntry(bucket['name'])
+    def readdir(self, path, offset=0):
+        if path == ".":
+            bucket = self.swift_conn.get_account()[1][offset]
+            yield (bucket['name'], self.getattr(bucket['name']), int(offset + 1))
 
-  
+def init_logging():
+    formatter = logging.Formatter('%(message)s') 
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+    log.setLevel(logging.INFO)    
+    log.addHandler(handler)    
+
 if __name__ == '__main__':  
-    fs = swiftfuse()  
-    fs.parse(errex=1)  
-    fs.main()  
+    init_logging()
+    sf = swiftfuse()
+    mountpoint = sys.argv[1]
+
+    llfuse.init(sf, mountpoint, [])
+    llfuse.main(single=True)
+
+    llfuse.close()
